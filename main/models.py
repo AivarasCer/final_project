3b295cfd8e1b8a2d8c4cef1b0478ea1393263781
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-10-30 13:08:08 +0200

    Add initial model.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MetaData(models.Model):
+    name = models.CharField(null=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    upload_at = models.DateField(null=True)
+    processing_time = models.TimeField(null=True)
+    chars = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.user} {self.upload_at} {self.processing_time} {self.chars}'
