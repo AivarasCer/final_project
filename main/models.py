2514a99c5aa732b8a2e5900b15e3e76dd5f45672
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-10-30 13:09:33 +0200

    Add migrations.

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class MetaData(models.Model):
-    name = models.CharField(null=False)
+    name = models.CharField(max_length=20, null=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     upload_at = models.DateField(null=True)
     processing_time = models.TimeField(null=True)
