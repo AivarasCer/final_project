a0322c569153312d69111a03b0489407193c70a9
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-10-31 14:15:17 +0200

    Fix forms.py

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 
-class UploadFileForm(forms.ModelForm):
+class UploadFileForm(forms.Form):
     LANGUAGE_CHOICES = [
         ('eng', 'English'),
         ('lit', 'Lithuanian')
     ]
     file = forms.FileField()
-    language = forms.ChoiceField(choices=LANGUAGE_CHOICES, initial='lit', widget=forms.RadioSelect)
+    language = forms.ChoiceField(choices=LANGUAGE_CHOICES, initial='eng', widget=forms.RadioSelect)
